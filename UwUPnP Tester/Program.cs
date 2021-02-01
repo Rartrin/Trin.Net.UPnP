20423171d433c5910612853cbacd1118be1da709
Rartrin <polliwhirl@gmail.com>
2021-01-31 17:46:07 -0800

    Added test for listing port mappings.

--- UwUPnP Tester/Program.cs
+++ UwUPnP Tester/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UwUPnP.Tester
 {
@@ -15,6 +16,8 @@ namespace UwUPnP.Tester
 				Console.WriteLine("It failed");
 			}
 			
+			PrintMappings();
+
 			const ushort port = 25000;
 			Console.WriteLine($"IsOpen({Protocol.TCP},{port}) --> {UPnP.IsOpen(Protocol.TCP, port)}");
 
@@ -24,10 +27,45 @@ namespace UwUPnP.Tester
 
 			Console.WriteLine($"IsOpen({Protocol.TCP},{port}) --> {UPnP.IsOpen(Protocol.TCP, port)}");
 
+			PrintMappings();
+
 			Console.ReadKey();
 			Console.WriteLine("Closing Port");
 			UPnP.Close(Protocol.TCP, port);
 			Console.WriteLine("Closed");
+		}
+
+
+		private static void PrintMappings()
+		{
+			Console.WriteLine("--Mappings Start--");
+			try
+			{
+				for(int i=0;;i++)
+				{
+					try
+					{
+						var ret = UPnP.GetGenericPortMappingEntry(i);
+						if(ret.Count == 0){break;}
+
+						Console.WriteLine(i);
+						foreach(var e in ret)
+						{
+							Console.WriteLine(e);
+						}
+						Console.WriteLine();
+					}
+					catch
+					{
+						break;
+					}
+				}
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e);
+			}
+			Console.WriteLine("--Mappings End--");
 		}
 	}
 }
